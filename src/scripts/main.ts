006213e52734ecd44d642c83c1554f1f0c9d7dff
Pavel <pavelmalyv@gmail.com>
2025-10-22 13:08:15 +0300

    feat: добавить header

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,6 @@
 import './shared/vendors';
+import { HeaderCollections } from './features/header';
 import { DialogCollections } from './features/dialog';
 
+new HeaderCollections();
 new DialogCollections();
