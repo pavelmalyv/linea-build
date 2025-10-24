80b709134455ed6fc35b8f6f62211cfb0766e350
Pavel <pavelmalyv@gmail.com>
2025-10-24 17:26:07 +0300

    feat: добавить секцию reviews

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,7 +2,9 @@ import './shared/vendors';
 import { HeaderCollections } from './features/header';
 import { DialogCollections } from './features/dialog';
 import { PortfolioCollections } from './features/portfolio';
+import { ReviewsCollections } from './features/reviews';
 
 new HeaderCollections();
 new DialogCollections();
 new PortfolioCollections();
+new ReviewsCollections();
